b2be546e01a1d9be804d74e5ab7f38680cd030b7
karabink <48476624+karabink@users.noreply.github.com>
2020-10-24 00:16:58 +0300

    Create Display.cs

---
+++ Assets/Scripts/Display.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Display : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text doorsText;
+    public Text livesText;
+
+    public GameObject messageText;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorsText.text = "Number of open / closed doors: " + Storage.Doors.ToString();
+        livesText.text = "Lives:" + Storage.Lives.ToString();
+        if (Storage.GameOver) 
+        {
+            (messageText).SetActive(true);
+        }
+    }
+}
