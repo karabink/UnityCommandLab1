576a7577168e55ecb337a520d807744bf57931fd
karabink <48476624+karabink@users.noreply.github.com>
2020-10-24 00:56:41 +0300

    Add files via upload

---
+++ Assets/Scripts/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Storage : MonoBehaviour
+{
+    public static int Doors = 0;
+    public static int Lives = 1;
+
+    public static bool GameOver = false;
+
+    public static void SetLives(int value)
+    {
+        Lives = value;
+        if (Lives == 0)
+        {
+            GameOver = true;
+            Time.timeScale = 0f;
+        };
+    }
+
+}
