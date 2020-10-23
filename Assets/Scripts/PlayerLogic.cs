9bebbf310ca27a5b77e0a33b1f050f85ca8094fd
karabink <48476624+karabink@users.noreply.github.com>
2020-10-24 01:13:47 +0300

    Add files via upload

---
+++ Assets/Scripts/PlayerLogic.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLogic : MonoBehaviour
+{
+    private Vector3 StartPos;
+    private Rigidbody rb;
+    public int Doors = 0;
+    
+    public void OpenClosed_doors()
+    {
+        Doors++;
+        print(Doors);
+        Storage.Doors++;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
